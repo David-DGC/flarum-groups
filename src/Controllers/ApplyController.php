3802ab6ab017680dbdbefab787421661cbefb9f8
David-DGC <dav10.poznanski@gmail.com>
2023-04-08 22:05:37 +0100

    Thanks, ChatGPT!
    Added stuff. Might not work.

--- src/Controllers/ApplyController.php
+++ src/Controllers/ApplyController.php
@@ -26,6 +26,7 @@ class ApplyController implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $code = Arr::get($request->getQueryParams(), 'code');
+        $action = Arr::get($request->getParsedBody(), 'action', 'join'); // Default action is 'join'
 
         /**
          * @var $invitation Invitation
@@ -48,13 +49,30 @@ class ApplyController implements RequestHandlerInterface
 
         $actor->assertCan('use', $invitation);
 
-        if (!$actor->groups->contains('id', $invitation->group->id)) {
-            $actor->groups()->save($invitation->group);
+        if ($action === 'join') {
+            if (!$actor->groups->contains('id', $invitation->group->id)) {
+                $actor->groups()->save($invitation->group);
 
-            $invitation->usage_count++;
-            $invitation->save();
+                $invitation->usage_count++;
+                $invitation->save();
 
-            $this->events->dispatch(new UsedInvitation($actor, $invitation));
+                $this->events->dispatch(new UsedInvitation($actor, $invitation));
+            }
+        } elseif ($action === 'leave') {
+            if ($actor->groups->contains('id', $invitation->group->id)) {
+                $actor->groups()->detach($invitation->group->id);
+
+                // Dispatch an event if necessary (You will need to create the LeftGroup event class)
+                // $this->events->dispatch(new LeftGroup($actor, $invitation));
+            } else {
+                throw new ValidationException([
+                    'group' => resolve(Translator::class)->trans('clarkwinkelmann-group-invitation.api.error.not-member'),
+                ]);
+            }
+        } else {
+            throw new ValidationException([
+                'action' => resolve(Translator::class)->trans('clarkwinkelmann-group-invitation.api.error.invalid-action'),
+            ]);
         }
 
         return new EmptyResponse();
